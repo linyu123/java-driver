fd641105ed200c395122c1cee2e49a7d9567284c
Andrew Tolbert <andrew.tolbert@datastax.com>
2017-01-11 18:18:30 -0600

    Prime keyspace metadata based on cassandra version

--- driver-core/src/test/java/com/datastax/driver/core/ScassandraCluster.java
+++ driver-core/src/test/java/com/datastax/driver/core/ScassandraCluster.java
@@ -19,6 +19,7 @@ import com.datastax.driver.core.utils.UUIDs;
 import com.google.common.collect.*;
 import org.scassandra.Scassandra;
 import org.scassandra.ScassandraFactory;
+import org.scassandra.cql.MapType;
 import org.scassandra.http.client.PrimingClient;
 import org.scassandra.http.client.PrimingRequest;
 import org.slf4j.Logger;
@@ -56,8 +57,11 @@ public class ScassandraCluster {
 
     private static final java.util.UUID schemaVersion = UUIDs.random();
 
-
     private final Map<Integer, Map<Integer, Map<String, Object>>> forcedPeerInfos;
+
+    private final String keyspaceQuery;
+
+    private final org.scassandra.http.client.types.ColumnMetadata[] keyspaceColumnTypes;
 
     ScassandraCluster(Integer[] nodes, String ipPrefix, int binaryPort, int adminPort,
                       List<Map<String, ?>> keyspaceRows, String cassandraVersion,
@@ -86,7 +90,30 @@ public class ScassandraCluster {
         }
         instances = instanceListBuilder.build();
         dcNodeMap = dcNodeMapBuilder.build();
-        this.keyspaceRows = keyspaceRows;
+
+        // Prime correct keyspace table based on C* version.
+        String[] versionArray = this.cassandraVersion.split("\\.|-");
+        double major = Double.parseDouble(versionArray[0] + "." + versionArray[1]);
+
+        if (major < 3.0) {
+            this.keyspaceQuery = "SELECT * FROM system.schema_keyspaces";
+            this.keyspaceColumnTypes = SELECT_SCHEMA_KEYSPACES;
+            this.keyspaceRows = Lists.newArrayList(keyspaceRows);
+            // remove replication map as it is not part of the < 3.0 schema.
+            for (Map<String, ?> keyspaceRow : this.keyspaceRows) {
+                keyspaceRow.remove("replication");
+            }
+        } else {
+            this.keyspaceQuery = "SELECT * FROM system_schema.keyspaces";
+            this.keyspaceColumnTypes = SELECT_SCHEMA_KEYSPACES_V3;
+            this.keyspaceRows = Lists.newArrayList(keyspaceRows);
+            // remove strategy options and strategy class as these are not part of the 3.0+ schema.
+            for (Map<String, ?> keyspaceRow : this.keyspaceRows) {
+                keyspaceRow.remove("strategy_class");
+                keyspaceRow.remove("strategy_options");
+            }
+        }
+
     }
 
     public Scassandra node(int node) {
@@ -363,11 +390,11 @@ public class ScassandraCluster {
                         .build())
                 .build());
 
-        // Prime keyspaces
+
         client.prime(PrimingRequest.queryBuilder()
-                .withQuery("SELECT * FROM system.schema_keyspaces")
+                .withQuery(keyspaceQuery)
                 .withThen(then()
-                        .withColumnTypes(SELECT_SCHEMA_KEYSPACES)
+                        .withColumnTypes(keyspaceColumnTypes)
                         .withRows(keyspaceRows)
                         .build())
                 .build());
@@ -447,6 +474,12 @@ public class ScassandraCluster {
             column("strategy_options", TEXT)
     };
 
+    static final org.scassandra.http.client.types.ColumnMetadata[] SELECT_SCHEMA_KEYSPACES_V3 = {
+            column("durable_writes", BOOLEAN),
+            column("keyspace_name", TEXT),
+            column("replication", MapType.map(TEXT, TEXT))
+    };
+
     static final org.scassandra.http.client.types.ColumnMetadata[] SELECT_SCHEMA_COLUMN_FAMILIES = {
             column("bloom_filter_fp_chance", DOUBLE),
             column("caching", TEXT),
@@ -515,36 +548,40 @@ public class ScassandraCluster {
         }
 
         public ScassandraClusterBuilder withSimpleKeyspace(String name, int replicationFactor) {
-            Map<String, Object> simpleKeyspaceRow = ImmutableMap.<String, Object>builder()
-                    .put("durable_writes", false)
-                    .put("keyspace_name", name)
-                    .put("strategy_class", "SimpleStrategy")
-                    .put("strategy_options", "{\"replication_factor\":\"" + replicationFactor + "\"}")
-                    .build();
-
+            Map<String, Object> simpleKeyspaceRow = Maps.newHashMap();
+            simpleKeyspaceRow.put("durable_writes", false);
+            simpleKeyspaceRow.put("keyspace_name", name);
+            simpleKeyspaceRow.put("replication", ImmutableMap.<String, String>builder()
+                    .put("class", "org.apache.cassandra.locator.SimpleStrategy")
+                    .put("replication_factor", "" + replicationFactor).build());
+            simpleKeyspaceRow.put("strategy_class", "SimpleStrategy");
+            simpleKeyspaceRow.put("strategy_options", "{\"replication_factor\":\"" + replicationFactor + "\"}");
             keyspaceRows.add(simpleKeyspaceRow);
             return this;
         }
 
         public ScassandraClusterBuilder withNetworkTopologyKeyspace(String name, Map<Integer, Integer> replicationFactors) {
             StringBuilder strategyOptionsBuilder = new StringBuilder("{");
+            ImmutableMap.Builder<String, String> replicationBuilder = ImmutableMap.builder();
+            replicationBuilder.put("class", "org.apache.cassandra.locator.NetworkTopologyStrategy");
+
             for (Map.Entry<Integer, Integer> dc : replicationFactors.entrySet()) {
                 strategyOptionsBuilder.append("\"");
                 strategyOptionsBuilder.append(datacenter(dc.getKey()));
                 strategyOptionsBuilder.append("\":\"");
                 strategyOptionsBuilder.append(dc.getValue());
                 strategyOptionsBuilder.append("\",");
+                replicationBuilder.put(datacenter(dc.getKey()), "" + dc.getValue());
             }
 
             String strategyOptions = strategyOptionsBuilder.substring(0, strategyOptionsBuilder.length() - 1) + "}";
 
-            Map<String, Object> ntsKeyspaceRow = ImmutableMap.<String, Object>builder()
-                    .put("durable_writes", false)
-                    .put("keyspace_name", name)
-                    .put("strategy_class", "NetworkTopologyStrategy")
-                    .put("strategy_options", strategyOptions)
-                    .build();
-
+            Map<String, Object> ntsKeyspaceRow = Maps.newHashMap();
+            ntsKeyspaceRow.put("durable_writes", false);
+            ntsKeyspaceRow.put("keyspace_name", name);
+            ntsKeyspaceRow.put("strategy_class", "NetworkTopologyStrategy");
+            ntsKeyspaceRow.put("strategy_options", strategyOptions);
+            ntsKeyspaceRow.put("replication", replicationBuilder.build());
             keyspaceRows.add(ntsKeyspaceRow);
             return this;
         }
